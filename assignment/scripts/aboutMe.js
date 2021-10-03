f016c75858cc986395350e82ab6f06c3069381ff
Jerome Horton <jeromehorton1@gmail.com>
2021-10-02 20:11:16 -0500

    updating assignment/scripts/aboutMe.js

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,21 +1,42 @@
+console.log( 'js' );
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = 'Jerome';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Horton';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+// We can create a third variable and assign it the value 'fullName' or we can use the existing
+// variables of our first and last name by concatenating '+' it in a sentence.
+let fullName = 'Jerome Horton'
+let luckyNumber = 8;
+let adventurous = true; // boolean
+let food = 'Liberian Food';
+let pets = 2;
+let friendsPets = 1;
+const allowedPets = 2;
 // 4 - Console log the value of `fullName`
+
+console.log( firstName );
+console.log( lastName );
+console.log( fullName );
+console.log( luckyNumber );
+console.log( adventurous );
+console.log( food );
+console.log( pets );
+console.log( friendsPets );
+console.log( allowedPets );
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 
-// 6 - Console log this sentence, adding in the variables you created above: 
+// 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 
-// 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+console.log( 'My name is ' + fullName + ' ,and I think ' + luckyNumber + ' is a winner!' );
 
+// 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+// *Revisit this*
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
@@ -26,29 +47,51 @@
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
-// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+if( adventurous ){
+  console.log( 'Adventures are great!' );
+}
+else {
+  console.log( 'How about we stay home?' );
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if( luckyNumber == adventurous ){
+  console.log( 'Think about it' );
+}
+else if ( luckyNumber == '2' || adventurous == 'true') {
+  console.log( 'Roll the dice!');
+}
+else {
+  console.log( 'Do not roll the dice!');
+}
 
-// 15 - Write a conditional that console logs "I can have more pets!" 
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
+if( pets <= allowedPets ){
+  console.log( 'I can have more pets' );
+}
+else if ( luckyNumber == '2' || adventurous == 'true') {
+  console.log( 'Roll the dice!');
+}
+else {
+  console.log( 'Do not roll the dice!');
+}
 
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
 // correctly checks the `pets` and `friendsPets` variables, and
 // assigns the highest value to `mostPets`. There's several possibilities --
-// be sure to think through all the scenarios. 
+// be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
 
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
